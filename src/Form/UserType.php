250ef92538eab41f5f61ba4f0e2c360a57f3aa6a
Morel <morel.jonathan@hotmail.fr>
2020-06-15 19:31:07 +0200

    edition profils prod et client

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,24 +4,49 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File as ConstraintsFile;
+use Symfony\Component\Validator\Constraints\IsFalse;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('photo')
-            ->add('phone')
-            ->add('address')
-            ->add('company')
-        ;
+            //->add('email')
+            //->add('roles')
+            //->add('password')
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('photoupload', FileType::class, [
+                'data_class' => null,
+                'mapped' => false,
+                'label' => 'Ajoutez/modifiez votre photo',
+                'required' => false,
+                'constraints' => [
+                    new ConstraintsFile([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png'
+                        ],
+                        'mimeTypesMessage' => "Merci d'uploader un fichier en .jpg ou png !",
+                    ])
+                ],
+                
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Telephone'
+            ])
+            ->add('address', AddressType::class)
+            ->add('company', CompanyType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
