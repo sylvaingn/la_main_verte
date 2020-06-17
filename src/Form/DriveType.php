d45b10e1c86318f93c87f900e9800abc56c1019d
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-06-17 23:02:21 +0200

    interface gestion point relais

--- src/Form/DriveType.php
+++ src/Form/DriveType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Drive;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,12 @@ class DriveType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('companies')
-            ->add('address')
+            ->add('name', TextType::class,[
+                'label' => 'Nom du point relais'
+            ])
+            ->add('address', AddressType::class, [
+                'label' => 'Addresse',
+            ])
         ;
     }
 
